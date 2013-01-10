9a098e30ce97147edd771bd912ab639d1aecafc7
Espen A. Kristiansen <post@espenak.net>
2013-01-10 21:05:09 +0100

    Works with devserver.

--- buildout/README.rst
+++ buildout/README.rst
@@ -1,11 +1,24 @@
 .. _buildout-init:
 
-Intialize
-=========
 
+Requirements
+============
+
+Virtualenv
+----------
+TODO
+
+PostgresSQL (or another database supported by Django)
+-----------------------------------------------------
+http://packages.python.org/psycopg2/install.html#install-from-source
+TODO: Not using postgres section (``parts -= psycopg2``)
+
+
+Configure buildout
+==================
 Create a directory that will be used to configure Devilry::
 
-    $ mkdir devilry
+    $ mkdir devilrybuild
 
 Create a configuration file named ``buildout.cfg`` in the directory. Add the
 following to the configuration file::
@@ -13,13 +26,18 @@ following to the configuration file::
     [buildout]
     extends = buildout-base.cfg
 
-Then CD to the directory and run the following commands to download Devilry and
+
+
+Intialize
+=========
+
+CD to the directory and run the following commands to download Devilry and
 all dependencies into a Python virtualenv. The end result is a
 selfcontained devilry buildout that only depends on the availability of a 
 compatible Python interpreter to run. The virtualenv is not affected by
 other Python packages installed globally::
 
-    $ cd devilry/
+    $ cd devilrybuild/
     $ mkdir -p buildoutcache/dlcache
     $ virtualenv --no-site-packages .
     $ bin/easy_install zc.buildout
@@ -33,11 +51,76 @@ and django config files along with the files and the database to be able to
 re-deploy an exact copy of the system.
 
 
+Configure Devilry
+=================
+To configure Devilry, you need to create a Python module containing a
+config-file named ``devilry_prod_settings.py``. First create a directory for
+your Devilry configurations::
+
+    $ mkdir /etc/devilry
+
+turn the directory into a Python module::
+
+    $ touch /etc/devilry/__init__.py
+
+and add your own ``devilry_production_settings.py`` to the directory::
+
+    TODO
+
+
+Create the database
+===================
+When you have configured a database, you can use the following command to 
+create your database::
+
+    $ cd /path/to/devilrybuild
+    $ bin/django.py syncdb
+
+The script will ask you to create a superuser. Choose a strong password - this
+user will have complete access to everything in Devilry.
+
+
+Test the install
+================
+To test that everything works as expected, you can use the Django devserver. First,
+enable debug-mode in your ``devilry_production_settings.py``::
+
+    DEBUG = True
+
+Then run the devserver::
+
+    $ bin/django.py runserver
+
+and open http://localhost:8000. You can tell the testserver to allow external
+connections, and to listen on another port with::
+
+    $ bin/django.py runserver 0.0.0.0:9000 --insecure
+
+.. warning::
+    NEVER use the devserver or ``DEBUG=True`` in production. It is insecure and
+    slow.
+
+.. note::
+    Some browsers have issues with loading the Devilry javascript sources
+    from the devserver. We recommend that you use a recent version of
+    Chrome, Firefox or Safari if you have problems.
+
+
+Setup Devilry for production
+============================
+Collect all static files in the ``static/``-subdirectory::
+
+    $ bin/django.py collectstatic
+
+
+
 Update devilry
 ==============
 
-1. Update the ``rev`` in the ``[devilry]`` section of your ``buildout.cfg``. The
-   ``rev`` is a GIT devision ID.
+1. Update the ``extends``-attribute in the ``[buildout]`` section of your
+   ``buildout.cfg``. The last path-segment before ``buildout-base.cfg``
+   is the GIT revision (CommmitID, branch or tag).
 2. Run buildout::
 
-       bin/buildout "buildout:parts=download-devilryrepo" && bin/buildout
+       $ bin/buildout "buildout:parts=download-devilryrepo" && bin/buildout
+       $ bin/django.py collectstatic --noinput
--- buildout/devilry_confexample/devilry_prod_settings.py
+++ buildout/devilry_confexample/devilry_prod_settings.py
@@ -1,12 +1,19 @@
+from os.path import join, abspath, dirname
+
 # Import the default settings from devilry
 from devilry_settings.default_settings import *
+
+# The directory containing _this_ file - useful if you configure paths relative to this directory
+this_dir = abspath(dirname(__file__))
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = '+g$%**q(w78xqa_2)(_+%v8d)he-b_^@d*pqhq!#2p*a7*9e9h'
 
 ## Nice to have this set to True while you are setting up devilry, however set
 ## it to False for production
-DEBUG = False
+DEBUG = True
+EXTJS4_DEBUG = DEBUG
+TEMPLATE_DEBUG = DEBUG
 
 
 #############################################
@@ -15,29 +22,31 @@ DEBUG = False
 DATABASES = {}
 
 ## Example config for SQLite (see also PostgreSQL below)
-#DATABASES["default"] = {
-    #'ENGINE': 'django.db.backends.sqlite3',
+DATABASES["default"] = {
+    'ENGINE': 'django.db.backends.sqlite3',
 
-    ## Path to sqlite database file - created by syncdb if it does not exist
+    # Path to sqlite database file - created by syncdb if it does not exist
     #'NAME': '/path/to/mydb.sqlite3'
-#}
+    'NAME': join(this_dir, 'mydb.sqlite3') # put the DB in the same dir as this file
+}
 
 ## Example config for PostgreSQL
-DATABASES["default"] = {
-   'ENGINE': 'django.db.backends.postgresql_psycopg2',
-   'NAME': 'devilry', # Name of the database
-   'USER': 'devilryuser', # Database user
-   'PASSWORD': 'supersecret', # Password of database user
-   'HOST': '', # Set to empty string for localhost.
-   'PORT': '', # Set to empty string for default.
-}
+#DATABASES["default"] = {
+   #'ENGINE': 'django.db.backends.postgresql_psycopg2',
+   #'NAME': 'devilry', # Name of the database
+   #'USER': 'devilryuser', # Database user
+   #'PASSWORD': 'supersecret', # Password of database user
+   #'HOST': 'localhost',
+   #'PORT': '', # Set to empty string for default.
+#}
 
 
 ###############################
 # Configure file upload storage
 ###############################
 ## Where do we store files that students deliver?
-DEVILRY_FSHIERDELIVERYSTORE_ROOT = '/devilry-filestorage'
+#DEVILRY_FSHIERDELIVERYSTORE_ROOT = '/var/devilry-filestorage'
+DEVILRY_FSHIERDELIVERYSTORE_ROOT = join(this_dir, 'filestorage')  # put the files in a subdirectory of the directory containing this file
 
 
 #####################################################################
@@ -45,7 +54,7 @@ DEVILRY_FSHIERDELIVERYSTORE_ROOT = '/devilry-filestorage'
 #####################################################################
 ## You can dump students and examiners into devilry. This setting is where
 ## the system tells users that this data comes from.
-DEVILRY_SYNCSYSTEM = 'FS (Felles Studentsystem)'
+#DEVILRY_SYNCSYSTEM = 'FS (Felles Studentsystem)'
 
 
 ##################################################################################
@@ -60,17 +69,15 @@ DEVILRY_JAVASCRIPT_LOCALE_OVERRIDE_APPS = ('devilry_university_translations',)
 # - The default is to "send" emails to stdout. This is useful when
 #   checking if email works, however you should change it to SMTP in production
 #######################################################
-
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 #EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 #EMAIL_HOST = 'smtp.example.com'
 #EMAIL_PORT = 25
 
 ## Email addresses
-DEVILRY_EMAIL_DEFAULT_FROM = 'devilry-support@example.com'
-DEVILRY_SYSTEM_ADMIN_EMAIL = 'devilry-admin@example.com'
+#DEVILRY_EMAIL_DEFAULT_FROM = 'devilry-support@example.com'
+#DEVILRY_SYSTEM_ADMIN_EMAIL = 'devilry-admin@example.com'
 
 ## The urlscheme+domain where devilry is located. Used when sending links to users via email.
 ## DEVILRY_SCHEME_AND_DOMAIN+DEVILRY_URLPATH_PREFIX is the absolute URL to the devilry
 ## instance. WARNING: must not end with /
-DEVILRY_SCHEME_AND_DOMAIN = 'https://devilry.example.com'
+#DEVILRY_SCHEME_AND_DOMAIN = 'https://devilry.example.com'
